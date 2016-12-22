3b566c194625815425d4ad286ad66f555de81108
ldh <229292620@qq.com>
2016-12-22 10:49:17 +0800

    init

---
+++ dhdb.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"Redico"
+	"log"
+	"os"
+	"path/filepath"
+	"flag"
+	"fmt"
+	"runtime"
+	"os/signal"
+	"strconv"
+)
+
+var dhdbVersion = "0.0.1"
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	var (
+		port = flag.Int("p", 6380, "dhdb port munber")
+		pass = flag.String("s", "icoolpy.com", "dhdb client auth password")
+	)
+	flag.Parse()
+
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	redServer, err := Redico.Run(dir,strconv.Itoa(*port))
+	if err != nil {
+		panic(err)
+	}
+	defer redServer.Close()
+	redServer.RequireAuth(*pass)
+	fmt.Println("DHDB Version:", dhdbVersion)
+	fmt.Println("DHDB Port:", strconv.Itoa(*port))
+	fmt.Println("Power By ICOOLPY.COM")
+
+	signalChan := make(chan os.Signal, 1)
+	cleanupDone := make(chan bool)
+	signal.Notify(signalChan, os.Interrupt)
+	go func() {
+		for _ = range signalChan {
+			cleanupDone <- true
+		}
+	}()
+	<-cleanupDone
+	fmt.Println("\nStoped DHDB...\n")
+}
---
+++ src/Redico/db.go
@@ -0,0 +1,129 @@
+package Redico
+
+import (
+	"strconv"
+	"sync"
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/opt"
+	"github.com/syndtr/goleveldb/leveldb/util"
+	"bytes"
+)
+
+// RedisDB holds a single (numbered) Redis database.
+type RedicoDB struct {
+	master  *sync.Mutex // pointer to the lock
+	id      int         // db id
+	leveldb *leveldb.DB
+	DbPath  string      //db path
+}
+
+func newRedicoDB(id int, l *sync.Mutex) RedicoDB {
+	rdb := RedicoDB{
+		id:            id,
+		master:        l,
+		DbPath: datapath + "/data/" + strconv.Itoa(id),
+	}
+	o := &opt.Options{
+		DisableBlockCache:      true,
+		BlockRestartInterval:   5,
+		BlockSize:              80,
+		Compression:            opt.NoCompression,
+		OpenFilesCacheCapacity: -1,
+		Strict:                 opt.StrictAll,
+		WriteBuffer:            1000,
+		CompactionTableSize:    2000,
+	}
+	ndb, err := leveldb.OpenFile(rdb.DbPath, o)
+	if err != nil {
+		panic(err)
+	}
+	rdb.leveldb = ndb
+	return rdb
+}
+
+func (db *RedicoDB) exists(k string) bool {
+	ok, _ := db.leveldb.Has([]byte(k), nil)
+	return ok
+}
+
+//var bufPool = sync.Pool{
+//	New:func() interface{} {
+//		buf := make([]byte, 8)
+//		return buf
+//	},
+//}
+
+// change int key value
+func (db *RedicoDB) stringIncr(k string, delta int) (int, error) {
+	v := 0
+	sv, err := db.leveldb.Get([]byte(k), nil)
+	if err != nil {
+		return 0, ErrKeyNotFound
+	}
+	v, err = strconv.Atoi(string(sv))
+	if err != nil {
+		return 0, ErrIntValueError
+	}
+	v += delta
+	db.stringSet(k, strconv.Itoa(v))
+	return v, nil
+}
+
+// allKeys returns all keys. Sorted.
+func (db *RedicoDB) allKeys() []string {
+	var keys []string
+	iter := db.leveldb.NewIterator(nil, nil)
+	defer iter.Release()
+	for iter.Next() {
+		keys = append(keys, string(iter.Key()))
+	}
+	//sort.Strings(keys) // To make things deterministic.
+	return keys
+}
+
+func (db *RedicoDB) keyStart(k string) []string {
+	var keys []string
+	iter := db.leveldb.NewIterator(util.BytesPrefix([]byte(k)), nil)
+	defer iter.Release()
+	for iter.Next() {
+		keys = append(keys, string(iter.Key()))
+	}
+	return keys
+}
+
+func (db *RedicoDB) keyRange(min string, max string) []string {
+	var keys []string
+	iter := db.leveldb.NewIterator(nil, nil)
+	defer iter.Release()
+	for ok := iter.Seek([]byte(min)); ok && bytes.Compare(iter.Key(), []byte(max)) <= 0; ok = iter.Next() {
+		keys = append(keys, string(iter.Key()))
+	}
+	return keys
+}
+
+func (db *RedicoDB) del(k string, delTTL bool) {
+	if !db.exists(k) {
+		return
+	}
+	err := db.leveldb.Delete([]byte(k), nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// stringGet returns the string key or "" on error/nonexists.
+func (db *RedicoDB) stringGet(k string) string {
+	data, err := db.leveldb.Get([]byte(k), nil)
+	if err != nil {
+		return ""
+	}
+	return string(data)
+}
+
+// stringSet force set()s a key. Does not touch expire.
+func (db *RedicoDB) stringSet(k, v string) {
+	err := db.leveldb.Put([]byte(k), []byte(v), nil)
+	if err != nil {
+		panic(err)
+	}
+}
